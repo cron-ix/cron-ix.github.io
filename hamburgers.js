08d202d9b730156d76fe03859e4c98d1728adfef
cron-ix <74101164+cron-ix@users.noreply.github.com>
2021-08-25 10:06:03 +0200

    fixed js

--- hamburgers.js
+++ hamburgers.js
@@ -1,8 +1,18 @@
 // toggle is-active class name using jQuery
-<script>
-  var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
-  });
-</script>
+/*
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active");
+  // Do something else, like open/close menu
+});
+*/
+
+// toggle is-active class name using JavaScript
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
