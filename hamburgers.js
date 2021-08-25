6980161cfbdd2c1855b2f197b1f3c9586736bbfc
cron-ix <74101164+cron-ix@users.noreply.github.com>
2021-08-25 09:51:53 +0200

    toggle is-active class name on hamburger

---
+++ hamburgers.js
@@ -0,0 +1,8 @@
+// toggle is-active class name using jQuery
+<script>
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
+</script>
